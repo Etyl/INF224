f0f254f170a92a81c2e81428adae9dc6b150302a
Hippolyte Verninas <etyl.sani@gmail.com>
2024-02-10 23:54:15 +0100

    add java client

--- swing/MainWindow.java
+++ swing/MainWindow.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 public class MainWindow extends JFrame {
     private static final long serialVersionUID = 1L;
     private JTextArea textArea;
@@ -54,6 +55,55 @@ public class MainWindow extends JFrame {
         buttonPanel.add(button2);
         buttonPanel.add(exitButton);
         add(buttonPanel, BorderLayout.SOUTH);
+
+        // Create the menu bar
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        // Create the menu
+        JMenu menu = new JMenu("Menu");
+        menuBar.add(menu);
+
+        // Create the menu items
+        JMenuItem menuItem1 = new JMenuItem("Button 1");
+        JMenuItem menuItem2 = new JMenuItem("Button 2");
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+
+        // Add action listeners to the menu items
+        menuItem1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textArea.append("Text from Button 1\n");
+            }
+        });
+
+        menuItem2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textArea.append("Text from Button 2\n");
+            }
+        });
+
+        exitMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        // Add menu items to the menu
+        menu.add(menuItem1);
+        menu.add(menuItem2);
+        menu.add(exitMenuItem);
+
+        // Create the toolbar
+        JToolBar toolBar = new JToolBar();
+        add(toolBar, BorderLayout.NORTH);
+
+        // Add buttons to the toolbar
+        toolBar.add(button1);
+        toolBar.add(button2);
+        toolBar.add(exitButton);
 
         // Set the size and make the window visible
         pack();
